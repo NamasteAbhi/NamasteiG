36b335cacd495a91d2261ca56c3bae0f85ac1ab7
Abhinav Bhardwaj <95855028+NamasteAbhi@users.noreply.github.com>
2022-12-14 02:52:48 +0530

    Add files via upload

---
+++ example.py
@@ -0,0 +1,15 @@
+from NamasteiG import Instagram
+
+#For Login IG Account
+UserName=''
+PassWord=''
+IG=Instagram(UserName,PassWord)
+IG.Login()
+
+#For Scrape Victim Followers
+VictimUserId=''
+data=IG.Scrape_Followers(VictimUserId)
+print(data)
+
+#More Features Comming Soon And Tell Me Which Types Of Features You Want To Add In This Library
+# Drop Your Suggestion Here => https://t.me/namastehackersgroup
