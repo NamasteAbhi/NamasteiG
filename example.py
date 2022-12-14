3b30a2c748950d0bbdfa676d712d7552139dfc14
Abhinav Bhardwaj <95855028+NamasteAbhi@users.noreply.github.com>
2022-12-15 00:44:48 +0530

    Update example.py

--- example.py
+++ example.py
@@ -11,5 +11,11 @@ VictimUserId=''
 data=IG.Scrape_Followers(VictimUserId)
 print(data)
 
+#For Scrape Victim Followings
+VictimUserId=''
+data=IG.Scrape_Followings(VictimUserId)
+print(data)
+
+
 #More Features Comming Soon And Tell Me Which Types Of Features You Want To Add In This Library
 # Drop Your Suggestion Here => https://t.me/namastehackersgroup
